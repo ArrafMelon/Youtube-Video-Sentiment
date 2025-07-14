2fdc4be6f750b4c46ce2d7ffc30aa58eb7c5536f
Arraf Khan <araf7791@gmail.com>
2025-07-13 22:16:20 -0400

    Wrote sentiment score script

---
+++ src/utils/sentimentScore.py
@@ -0,0 +1,43 @@
+import commentScraper
+from googleapiclient.discovery import build 
+import sys
+from dotenv import load_dotenv
+import os
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import torch
+
+# Google API Key
+load_dotenv()
+api_key = os.getenv("YOUTUBE_API_KEY")
+
+# User input
+#if len(sys.argv) != 2:
+#    print("Add a video ID after the script name")
+#    sys.exit(1)
+    
+#vid_id = sys.argv[1]
+
+vid_id = "EYgnaNx9xtI"
+
+yt_client = build("youtube", "v3", developerKey=api_key)
+
+def get_score(vid_id, client=yt_client):
+    comment_df = commentScraper.comments_df(client,vid_id).sort_values(by="Number of Likes", ascending=False)[:100].drop_duplicates()
+
+    tokenizer = AutoTokenizer.from_pretrained('cardiffnlp/twitter-roberta-base-sentiment')
+    model = AutoModelForSequenceClassification.from_pretrained('model', local_files_only=True)
+    model.eval()
+
+    text = comment_df["Comment"].tolist()
+
+    # Run inference
+    inputs = tokenizer(text, return_tensors="pt",padding=True, truncation=True)
+
+    with torch.no_grad():
+        logits = model(**inputs).logits
+        prediction = torch.argmax(logits, dim=1)
+
+    comment_df["Predicted_class"] = prediction.numpy()
+    return comment_df["Predicted_class"].value_counts()
+
+print(get_score(vid_id))
